d29fd7a1d6483bec4ffc84f4011a6a451d6e9b35
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-12 20:17:03 +0530

    Using app module to provide service (account service) everywhere.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { AccountService } from './account.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AccountService],
 })
 export class AppComponent implements OnInit {
   accounts: { name: string; status: string }[] = [];
@@ -13,6 +12,6 @@ export class AppComponent implements OnInit {
   constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.accounts = this.accountService.accounts; 
+    this.accounts = this.accountService.accounts;
   }
 }
