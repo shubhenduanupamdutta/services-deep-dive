55298b9a0f2aa0f7adfd0089dfdf17746686b177
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-03-12 20:24:06 +0530

    Inject a service into another one using providers in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { AccountService } from './account.service';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { AccountService } from './account.service';
     BrowserModule,
     FormsModule,
   ],
-  providers: [AccountService],
+  providers: [AccountService, LoggingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
